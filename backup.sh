051c4ac0259c653129d1397b85d42e68d6c771bb
Glitch (nz-covid-scan-data) <none>
2021-11-05 01:29:03 +0000

    Switching backup approach to keep dump files smaller

--- backup.sh
+++ backup.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-sqlite3 .data/database.sqlite .dump > database.dump; git commit -am'Latest db backup'; git push
+sqlite3 .data/database.sqlite .dump > database.dump; 
+
+
+backupFolder="backup"
+mkdir -p $backupFolder
+
+input_start=2021-09-01
+# input_end=
+
+# After this, startdate and enddate will be valid ISO 8601 dates,
+# or the script will have aborted when it encountered unparseable data
+# such as input_end=abcd
+startdate=$(date -I -d "$input_start") || exit -1
+enddate=$(date -I)     || exit -1
+
+d="$startdate"
+while [ "$d" != "$enddate" ]; do 
+  echo $d
+  cat database.dump | grep $d > $backupFolder/$d.dump
+  d=$(date -I -d "$d + 1 day")
+done
+
+
+git add backup;
+git commit -m'Latest db backup'; git push
+
